ce48da57ced9da86fbbc466c117fefded49a7652
Manchalasarika <sarikamanchala33@gmail.com>
2021-10-27 20:03:34 -0500

    updated

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var mydataRouter = require('./routes/mydata');
+var mathRouter = require('./routes/math');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mydata', mydataRouter);
+app.use('/math', mathRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
